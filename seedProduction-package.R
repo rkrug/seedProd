d5c1c7ee0a7d21978ddb36c6c0094ec861eac742
Rainer M. Krug <R.M.Krug@gmail.com>
2013-04-25 14:30:22 +0200

    Initial commit

---
+++ seedProduction-package.R
@@ -0,0 +1,14 @@
+##' Seed production routines
+##' 
+##' \tabular{ll}{
+##' Package: \tab seedProduction\cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.0.1\cr
+##' Date: \tab 2013-04-25_14-29\cr
+##' License: \tab GPL (>= 2)\cr
+##' LazyLoad: \tab yes\cr
+##' }
+##'
+##' @docType package
+##' @author Rainer M Krug \email{Rainer@@krugs.de}
+NULL
